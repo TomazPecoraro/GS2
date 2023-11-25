3b9d44619dd7dd5be248c5557dc58d2e3c054541
FelipeSieiro <felipedossantos0804@gmail.com>
2023-11-24 21:44:20 -0300

    cabeçalho finalizado

---
+++ src/components/cabecalho/cabecalho.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import  styles from './cabecalho.module.css';
+import Image from "next/image";
+
+export default function Cabecalho() {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <div className={styles.cabecalho}>
+          <h1 className={styles.NOME}>
+            <Link href="/" className = {styles.link}>SKINSCAN</Link>
+            <Image src="/01.png" width={54}height={50}/>
+          </h1>
+          <div className={styles.botao}>
+            <Link href="/doencas/page" className = {styles.link}>Doenças de Pele</Link>
+          </div>
+          <div className={styles.botao}>
+            <Link href="/formulario/page" className = {styles.link}>Formulário</Link>
+          </div>
+          <div className={styles.botao}>
+            <Link href="/membros/page" className = {styles.link}>Membros</Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
