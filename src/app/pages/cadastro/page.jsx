6dfbcd0ceb58e64adb8744a3b628db2d86a42e4c
--global <--global>
2023-11-24 22:00:26 -0300

    cadastro ok

---
+++ src/app/pages/cadastro/page.jsx
@@ -0,0 +1,12 @@
+import '../../app/globals.css'
+import Cadastro from '../../components/cadastro/cadastro'
+
+export default function PGCadastro() {
+    return (
+    <div>
+    <section>
+        <Cadastro/>
+    </section>
+    </div>
+    )
+}
